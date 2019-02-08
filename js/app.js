a1ebefe138f747a9c347a05663a3e611b9f9d5fa
Masafir-Nova <lachamir@outlook.fr>
2019-02-08 13:51:12 +0100

    tryng to add bulma ext

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
 var app = {
     $dbName : '',
-    $formName:$('#addDbModal'),
+    $DbModal: $('#addDbModal'),
+    $tableModal : $('#addTableModal'),
+    $addTableBtn : $('#addTableBtn'),
     init : function () {
         console.log('coucou depuis l\'espace');
         
         
          $('#Db-create').on('submit', app.addNameModal);
-        /*$(window).on('load',function(){
+        app.$addTableBtn.on('click',app.addTableModal);
+         /*$(window).on('load',function(){
         console.log('gribouillis on load '); 
-    });*/
+        });*/
 
     },
+    //function use to display formModal for the DB
     addNameModal : function(event) {
         
         event.preventDefault();
         app.$dbName = $('.input').val();
         app.updateDbName();
-        app.$formName.removeClass('is-active');
-
-
+        app.$DbModal.removeClass('is-active');
 
     },
+    //function use to update dbname
     updateDbName : function(){
         
         $('#dbtitle').text(app.$dbName);
+    },
+    addTableModal: function(){
+        app.$tableModal.addClass('is-active');
     }
 };
 
